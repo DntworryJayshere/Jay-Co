fe8141c1dcb4960373104cfa4a97d266cceec487
Jay Rodriguez <jared.m.rodriguez@gmail.com>
2021-02-22 21:03:15 -0600

    updates to landing page, finalized design

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -1,10 +1,100 @@
 import React, { Component } from 'react';
-import Cards from '../layout/Cards';
 
 class Landing extends Component {
 	render() {
-		return <Cards />;
+		return (
+			<div className="outerContainer splashOuterContainer">
+				<div className="container">
+					<div className="row justify-content-center position-relative">
+						<div className="col-lg-6 text-center align-self-center topping">
+							<h2 className="splashHeading text-uppercase text-white text-left">
+								Jay&Co Scheduling App
+							</h2>
+							<br />
+							<p className="text-white splashText-left">
+								The Jay&Co Custom Scheduling App provides enterprise level
+								scheduling from the comfort and convenience of your own home.
+								One application works for all service industries, including
+								councelers, barbers, detailers, and doctors!
+							</p>
+						</div>
+						<div className="col text-center topping">
+							<div className="box">
+								<div className="splashPic1"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<br />
+				<div className="container ">
+					<div className="row justify-content-center splash-border position-relative">
+						<div className="col topping">
+							<div className="box">
+								<div className="splashPic2"></div>
+							</div>
+						</div>
+						<div className="col-lg-6 align-self-center text-center topping">
+							<h2 className="splashHeading text-uppercase text-white text-left">
+								Jay&Co Scheduling App Plus
+							</h2>
+							<br />
+							<p className="text-white splashText-left">
+								The Jay&Co Custom Scheduling App Plus provides the same
+								enterprise level scheduling as the standard application with the
+								added functionality of detailed scheduling analysis so that you
+								can more effeciently manange your time.
+							</p>
+						</div>
+					</div>
+				</div>
+				<br />
+				<div className="container">
+					<div className="row justify-content-center splash-border position-relative">
+						<div className="col-lg-6 text-center align-self-center topping">
+							<h2 className="splashHeading text-uppercase text-white text-left">
+								Jay&Co Scheduling App Support Plus
+							</h2>
+							<br />
+							<p className="text-white splashText-left">
+								The Jay&Co Custom Scheduling App Support Plus provides the same
+								functionality as the subservant tier with the added 24 hour
+								scheduling support that your small business so desperatly needs.
+								Dont worry about answering phones - we'll take care of it!
+							</p>
+						</div>
+						<div className="col text-center topping">
+							<div className="box">
+								<div className="splashPic3"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<br />
+				<div className="container">
+					<div className="row justify-content-center splash-border position-relative">
+						<div className="col topping">
+							<div className="box">
+								<div className="splashPic4"></div>
+							</div>
+						</div>
+						<div className="col-lg-6 align-self-center text-center topping">
+							<h2 className="splashHeading text-uppercase text-white text-left">
+								Jay&Co Scheduling App Plus Plus Plus
+							</h2>
+							<br />
+							<p className="text-white splashText-left">
+								You thought it wasnt possible to schedule like the Jay&Co Custom
+								Scheduling App Plus Plus Plus can. Not only does this
+								application schedule, but it also finds you jobs/clients that
+								pay your business well above the market rate. Its going to blow
+								your scheduling needs out of the water.
+							</p>
+						</div>
+					</div>
+				</div>
+				<br />
+			</div>
+		);
 	}
 }
-
 export default Landing;
--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import Button from 'react-bootstrap/Button';
 import Moment from 'react-moment';
 import moment from 'moment';
 import 'moment-timezone';
