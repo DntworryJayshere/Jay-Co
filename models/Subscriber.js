1378f115f42188635a46b5021ca05026bd469aec
Jay Rodriguez <jared.m.rodriguez@gmail.com>
2021-03-02 19:26:53 -0600

    inclusion of subscriber model

---
+++ models/Subscriber.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const subscriberSchema = new Schema({
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	date: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+const Subscriber = mongoose.model('Subscriber', subscriberSchema);
+module.exports = Subscriber;
